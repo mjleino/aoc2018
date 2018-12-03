dbea93083b3c900cfe4aff06044f2e51444454c0
Mikko Leino <mikko@viiksipojat.fi>
2018-12-03 08:40:33 +0100

    WIP: Day 2

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -22,7 +22,22 @@
    [(sorted-set 0), 0]
    (cycle (read-numbers filename))))
 
+(defn read-lines [filename]
+  (with-open [rdr (clojure.java.io/reader filename)]
+    (doall (line-seq rdr))))
+
+(defn count-twos-threes [label]
+  (let [freqs (vals (frequencies label))]
+    [(some true? (map #(= 2 %) freqs)) (some true? (map #(= 3 %) freqs))]))
+
+(defn sum-twos-threes [[total-twos total-threes] [twos threes]]
+  [(+ total-twos (if twos 1 0)) (+ total-threes (if threes 1 0))])
+
+(defn task-2-1 [filename]
+  (reduce sum-twos-threes [0 0] (map count-twos-threes (read-lines filename))))
+
 (defn -main []
-  (println "Day 1 (task 1):" (task-1-1 "resources/1-1.txt"))
-  (println "Day 1 (task 2):" (task-1-2 "resources/1-1.txt")))
+  ; (println "Day 1 (task 1):" (task-1-1 "resources/1-1.txt"))
+  ; (println "Day 1 (task 2):" (task-1-2 "resources/1-1.txt"))
+  (println "Day 2 (task 1):" (task-2-1 "resources/2-1.txt")))
 
