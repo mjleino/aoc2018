a8f54c95bc4c401e1c678e0bb45465986043a814
Mikko Leino <mikko@viiksipojat.fi>
2018-12-15 14:59:57 +0100

    Add tests to day 2 tasks

--- test/aoc/core_test.clj
+++ test/aoc/core_test.clj
@@ -9,3 +9,12 @@
 (deftest task-1-2-test
   (testing "Day 1 (task 2)"
     (is (= 367 (task-1-2 "resources/1-1.txt")))))
+
+(deftest task-2-1-test
+  (testing "Day 1 (task 1)"
+    (is (= 5750 (task-2-1 "resources/2-1.txt")))))
+
+(deftest task-2-2-test
+  (testing "Day 1 (task 2)"
+    (is (= "tzyvunogzariwkpcbdewmjhxi" (task-2-2 "resources/2-1.txt")))))
+
